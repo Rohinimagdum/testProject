136fa035a8839ef01cf2c1fc5209ebc550391e66
Rohinimagdum <magdumrohini97@gmail.com>
2022-01-14 10:34:00 -0800

    This is java practice file

---
+++ src/test/java/testProject/variables_java.java
@@ -0,0 +1,22 @@
+package testProject;
+
+public class variables_java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  int a=45;
+		     int b=50;
+		     System.out.println(a);
+		     System.out.println(b);
+		     System.out.println(a+b);
+		     System.out.println(a-b);
+		     System.out.println(a*b);
+		     System.out.println(a/b);
+		     System.out.println(a++);
+		     System.out.println(++a);
+		     System.out.println(a--);
+		     System.out.println(--a);
+			
+	}
+
+}
